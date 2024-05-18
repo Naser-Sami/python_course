62d12268ef492cb48da1d94ce8ee70829f96304f
Naser Sami <naser_ebedo@icloud.com>
2024-05-18 09:00:54 +0300

    modules

---
+++ 077_modules_two/create_your_module.py
@@ -0,0 +1,18 @@
+# -----------------------------------
+# -- Modules => Create Your Module --
+# -----------------------------------
+
+# import sys
+
+# print(sys.path)
+# sys.path.append("")     # add file in a desired path
+# print(sys.path)
+
+# import functions
+# from functions import welcome_back
+# from functions import welcome_back as w
+# from functions import *
+import functions as f
+
+print(f.welcome_back("Naser"))
+print(f.your_age(29))
---
+++ 077_modules_two/functions.py
@@ -0,0 +1,7 @@
+
+def welcome_back(name : str):
+    return f"Hello, {name}, welcome back!"
+
+
+def your_age(age : int):
+    return f"Your age is {age}"
